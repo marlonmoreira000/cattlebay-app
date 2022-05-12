dc7ba36a4a4ebe407e00891a2f1b3ef406788ec9
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-05-12 18:30:13 +0800

    add user, profile and listing models

--- db/migrate/20220512102935_create_listings.rb
+++ db/migrate/20220512102935_create_listings.rb
@@ -1,7 +1,7 @@
 class CreateListings < ActiveRecord::Migration[7.0]
   def change
     create_table :listings do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :profile, null: false, foreign_key: true
       t.string :name
       t.text :description
       t.integer :price
