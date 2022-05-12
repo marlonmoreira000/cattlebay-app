8b0ee3f198dd96a70830a801670db6ee9f34ead4
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-05-12 18:39:24 +0800

    fix db issue by deleting old db and resetting

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_095513) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_12_102935) do
   create_table "listings", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.integer "profile_id", null: false
     t.string "name"
     t.text "description"
     t.integer "price"
     t.boolean "purchased"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_listings_on_user_id"
+    t.index ["profile_id"], name: "index_listings_on_profile_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "username"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,13 +37,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_095513) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
-    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "listings", "users"
+  add_foreign_key "listings", "profiles"
+  add_foreign_key "profiles", "users"
 end
