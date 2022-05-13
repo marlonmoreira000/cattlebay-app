2b7fa3527314ddf681e720b16d64c46308fcd229
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-05-13 14:31:57 +0800

    add some basic authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :forbidden
+
+    def forbidden
+        # render plain: "working"
+        flash[:alert] = "You're not authorized to perform this action!"
+        # render json: flash
+        # render plain: listings_path
+        redirect_to root_path
+    end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,16 @@
 class ListingsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
+    before_action :check_auth
 
     def index
         @listings = Listing.all
     end
 
     def show
+    end
+
+    private
+    def check_auth
+        authorize Listing
     end
 end
