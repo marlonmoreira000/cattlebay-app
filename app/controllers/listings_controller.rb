e7306f0023945faac5e0801f9a2b02919250f0ef
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-05-13 20:14:27 +0800

    fix up user-profile relationship

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,35 +1,36 @@
 class ListingsController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
+    before_action :authenticate_user!, except: %i[index show]
     before_action :check_auth
 
     def index
         @listings = Listing.all
     end
 
-    def show
-    end
+    def show; end
 
     def new
         @listing = Listing.new
     end
 
     def create
-        @listing = Listing.create(listing_params)
-        if @listing.valid?
-        redirect_to @listing
-      else
-        flash.now[:alert] = @listing.errors.full_messages.join('<br>')
-        render 'new'
-      end
-        # render json: listing_params
+        # @listing = Listing.create(listing_params)
+        render json: current_user.profile
+        # if @listing.valid?
+        #     redirect_to @listing
+        # else
+        #     flash.now[:alert] = @listing.errors.full_messages.join('<br>')
+        #     render 'new'
+        # end
+      # render json: listing_params
     end
 
-    private
+  private
+
     def check_auth
         authorize Listing
     end
 
     def listing_params
     return params.require(:listing).permit(:name, :picture)
-  end
+    end
 end
