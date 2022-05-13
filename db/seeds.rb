634c70140b00f8b516d3675bfd891f87ff43a32a
marlonmoreira000 <marlonmoreira000@gmail.com>
2022-05-13 14:42:16 +0800

    restructure seeds file - without migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
 Listing.destroy_all
 Profile.destroy_all
 User.destroy_all
 
-p1 = Profile.create(user_id: User.create(email: "user1@gmail.com", password: "password1").id)
-p2 = Profile.create(user_id: User.create(email: "user2@gmail.com", password: "password2").id)
-p3 = Profile.create(user_id: User.create(email: "user3@gmail.com", password: "password3").id)
+u1 = User.create(email: "user1@gmail.com", password: "password1")
+u2 = User.create(email: "user2@gmail.com", password: "password2")
+u1.add_role :buyer
+u2.add_role :seller
+
+p1 = Profile.create(user_id: u1.id)
+p2 = Profile.create(user_id: u2.id)
+
+# p1 = Profile.create(user_id: User.create(email: "user1@gmail.com", password: "password1").id)
+# p2 = Profile.create(user_id: User.create(email: "user2@gmail.com", password: "password2").id)
 
 p1.listings.create(name: "spot", description: "a small blue heeler", price: 399, purchased: false)
 p1.listings.create(name: "simba", description: "a small red heeler", price: 399, purchased: false)
 p2.listings.create(name: "bonkers", description: "a big blue heeler", price: 299, purchased: false)
 p2.listings.create(name: "willow", description: "a big red heeler", price: 299, purchased: false)
-p3.listings.create(name: "cheeky", description: "a small working dog", price: 500, purchased: false)
-p3.listings.create(name: "oscar", description: "a big working dog", price: 500, purchased: false)
-
-
